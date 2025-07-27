b6a62058951c63f9330c792a95e2e906980b635e
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2025-07-27 12:24:43 +0330

    feat(back): users api route

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,128 @@
+import { updateUserSchema } from '@/features/users/schema'
+import { adminOnly, getCurrentUser } from '@/libs/auth'
+import { prisma } from '@/libs/prisma'
+import { Params } from '@/types/api'
+import { ApiResponseHandler } from '@/utils/apiResponse'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest, { params }: Params) {
+  try {
+    const user = await getCurrentUser(request)
+    const authResponse = adminOnly(user)
+
+    if (authResponse) return authResponse
+
+    const { id } = await params
+
+    const userDB = await prisma.user.findUnique({
+      where: { id: Number(id) },
+      omit: { password: true },
+      include: {
+        orders: {
+          include: {
+            items: {
+              select: {
+                book: {
+                  select: {
+                    id: true,
+                    title: true,
+                    price: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        reviews: {
+          select: {
+            id: true,
+            book: {
+              select: {
+                id: true,
+                title: true,
+              },
+            },
+            rating: true,
+            comment: true,
+            createdAt: true,
+          },
+        },
+      },
+    })
+
+    if (!userDB) {
+      return ApiResponseHandler.notFound('کاربری پیدا نشد')
+    }
+
+    return ApiResponseHandler.success(userDB)
+  } catch (error) {
+    return ApiResponseHandler.internalError(undefined, error)
+  }
+}
+
+export async function PUT(request: NextRequest, { params }: Params) {
+  try {
+    const user = await getCurrentUser(request)
+    const authResponse = adminOnly(user)
+
+    if (authResponse) return authResponse
+
+    const { id } = await params
+
+    const userDB = await prisma.user.findUnique({
+      where: { id: Number(id) },
+      select: { id: true },
+    })
+
+    if (!userDB) {
+      return ApiResponseHandler.notFound('کاربری پیدا نشد')
+    }
+
+    const body = await request.json()
+
+    const validationResult = updateUserSchema.safeParse(body)
+
+    if (!validationResult.success) {
+      return ApiResponseHandler.validationError(validationResult.error)
+    }
+
+    const updated = await prisma.user.update({
+      where: { id: userDB.id },
+      data: validationResult.data,
+    })
+
+    return ApiResponseHandler.success(updated, 'کاربر با موفقیت بروزرسانی شد')
+  } catch (error) {
+    return ApiResponseHandler.internalError(undefined, error)
+  }
+}
+
+export async function DELETE(request: NextRequest, { params }: Params) {
+  try {
+    const user = await getCurrentUser(request)
+    const authResponse = adminOnly(user)
+
+    if (authResponse) return authResponse
+
+    const { id } = await params
+
+    const userDB = await prisma.user.findUnique({
+      where: { id: Number(id) },
+      select: { id: true },
+    })
+
+    if (!userDB) {
+      return ApiResponseHandler.notFound('کاربری پیدا نشد')
+    }
+
+    const deleted = await prisma.$transaction([
+      prisma.user.delete({ where: { id: userDB.id } }),
+      prisma.order.deleteMany({ where: { userId: userDB.id } }),
+      prisma.review.deleteMany({ where: { userId: userDB.id } }),
+    ])
+
+    return ApiResponseHandler.success(deleted, 'کاربر با موفقیت حذف شد')
+  } catch (error) {
+    return ApiResponseHandler.internalError(undefined, error)
+  }
+}
---
+++ src/app/api/users/route.ts
@@ -0,0 +1,58 @@
+import { adminOnly, getCurrentUser } from '@/libs/auth'
+import { prisma } from '@/libs/prisma'
+import { SearchConfig } from '@/types/api'
+import { ApiResponseHandler } from '@/utils/apiResponse'
+import { PaginationHelper } from '@/utils/pagination'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  try {
+    const user = await getCurrentUser(request)
+    const authResponse = adminOnly(user)
+
+    if (authResponse) return authResponse
+
+    const { page, limit, skip, search, filters } = PaginationHelper.extractParams(request)
+
+    const validationError = PaginationHelper.validateParams(page, limit)
+
+    if (validationError) {
+      return ApiResponseHandler.error(validationError, 400)
+    }
+
+    const searchConfig: SearchConfig = {
+      searchFields: ['name', 'email', 'phone'],
+      filterFields: ['isActive'],
+    }
+    const where = PaginationHelper.buildWhereClause(search, filters, searchConfig)
+
+    const [data, total] = await prisma.$transaction([
+      prisma.user.findMany({
+        where,
+        skip,
+        take: limit,
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+          isActive: true,
+          createdAt: true,
+        },
+        orderBy: { createdAt: 'desc' },
+      }),
+      prisma.user.count({ where }),
+    ])
+
+    const meta = PaginationHelper.createMeta(total, page, limit, search, filters)
+
+    const response = {
+      data,
+      meta,
+    }
+
+    return ApiResponseHandler.success(response)
+  } catch (error) {
+    return ApiResponseHandler.internalError(undefined, error)
+  }
+}
