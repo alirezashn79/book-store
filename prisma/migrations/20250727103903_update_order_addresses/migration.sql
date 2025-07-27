3d0119d3f932964357fb935e8768edef0538a450
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2025-07-27 14:09:35 +0330

    chore(back): migration

---
+++ prisma/migrations/20250727103903_update_order_addresses/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Address" ADD COLUMN     "latitude" DOUBLE PRECISION,
+ADD COLUMN     "longitude" DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "addressId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address"("id") ON DELETE SET NULL ON UPDATE CASCADE;
