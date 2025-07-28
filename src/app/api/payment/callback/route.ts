fbcb356b51c677f6521f11fea8d7392f66cdf049
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2025-07-29 00:34:04 +0330

    refactor(back): payment api route

--- src/app/api/payment/callback/route.ts
+++ src/app/api/payment/callback/route.ts
@@ -1,32 +1,47 @@
+import { getCurrentUser } from '@/libs/auth'
 import { prisma } from '@/libs/prisma'
 import { ApiResponseHandler } from '@/utils/apiResponse'
 import { NextRequest } from 'next/server'
 
 export async function POST(request: NextRequest) {
   try {
+    const user = await getCurrentUser(request)
+
+    if (!user) {
+      return ApiResponseHandler.unauthorized()
+    }
     const { transactionId, orderId, status, reference } = await request.json()
 
-    const [updatedTransaction, updatedOrder] = await prisma.$transaction([
-      prisma.transaction.update({
-        where: { id: transactionId },
+    await prisma.$transaction(async (tx) => {
+      await tx.transaction.update({
+        where: { id: Number(transactionId), userId: Number(user.id) },
         data: {
           status: status === 'success' ? 'COMPLETED' : 'FAILED',
           reference,
           transactionDate: new Date(),
         },
-      }),
-      prisma.order.update({
-        where: { id: orderId },
+      })
+
+      await tx.order.update({
+        where: { id: Number(orderId), userId: Number(user.id) },
         data: {
           status: status === 'success' ? 'PROCESSING' : 'CANCELLED',
         },
-      }),
-    ])
+      })
 
-    return ApiResponseHandler.success({
-      order: updatedOrder,
-      transaction: updatedTransaction,
+      const cart = await tx.cart.findUnique({
+        where: {
+          userId: user.id,
+        },
+        select: { id: true },
+      })
+
+      if (cart) {
+        await tx.cartItem.deleteMany({ where: { cartId: cart.id } })
+      }
     })
+
+    return ApiResponseHandler.success(undefined, 'سفارش با موفقیت تایید شد')
   } catch (error) {
     return ApiResponseHandler.internalError(undefined, error)
   }
